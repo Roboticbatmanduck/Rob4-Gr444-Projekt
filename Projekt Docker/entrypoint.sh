4cd9f88542c866b2cfaf3b394c5ddcd42c926c1d
Orin <amphoros27@gmail.com>
2026-04-16 08:39:43 +0200

    MODIFIED: Jetson Docker

--- Projekt Docker/entrypoint.sh
+++ Projekt Docker/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-# Source ROS 2 Jazzy
-source /opt/ros/jazzy/setup.bash
+# Source ROS 2 Humble
+source /opt/ros/humble/setup.bash
 
 # Source workspace hvis det findes
 if [ -f /workspace/install/setup.bash ]; then
   source /workspace/install/setup.bash
+else 
+	echo "Ros setup not found!" >&2
+	exit 1
 fi
 
 exec "$@"
