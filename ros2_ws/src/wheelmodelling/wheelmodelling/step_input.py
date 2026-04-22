b8e4587665f5446b464c6422f3b6d8d718924e13
roboticbatmanduck <jlinde24@student.aau.dk>
2026-04-22 09:17:52 +0200

    ADD: modelling

--- ros2_ws/src/wheelmodelling/wheelmodelling/step_input.py
+++ ros2_ws/src/wheelmodelling/wheelmodelling/step_input.py
@@ -3,7 +3,7 @@ import os, json
 from rclpy.node import Node
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import JointState
+from sensor_msgs.msg import JointState, Imu
 from geometry_msgs.msg import TwistStamped, Twist
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 from ament_index_python.packages import get_package_share_directory
@@ -35,6 +35,7 @@ class StepInputNode(Node):
         self.declare_parameter('duration', cfg.get('duration', 5.0)) #s
         self.declare_parameter('odom_topic', '/odom')
         self.declare_parameter('joint_state_topic', '/joint_states')
+        self.declare_parameter('imu_topic', '/imu')
         #Get parameters
         p = self.get_parameter
         self.cmd_topic = p('cmd_topic').value
@@ -45,6 +46,7 @@ class StepInputNode(Node):
         self.duration = float(p('duration').value)
         self.odom_topic = self.get_parameter('odom_topic').value
         self.joint_topic = self.get_parameter('joint_state_topic').value
+        self.imu_topic = self.get_parameter('imu_topic').value
         #Publisher and timer
         #send twist command to cmd_topic with 10 up to backlog
         self.publisher = self.create_publisher(Twist, self.cmd_topic, 10)
@@ -59,8 +61,9 @@ class StepInputNode(Node):
                                 depth=10)
         
         #Subscriber til /odom topiccen
-        #self.subscriber = self.create_subscription(Odometry, self.odom_topic, self._on_odom, qos_sensor)
-        self.subscriber = self.create_subscription(JointState, self.joint_topic, self._on_joint_states, qos_sensor)
+        self.subscriber_odom = self.create_subscription(Odometry, self.odom_topic, self._on_odom, qos_sensor)
+        self.subscriber_joint = self.create_subscription(JointState, self.joint_topic, self._on_joint_states, qos_sensor)
+        self.subscriber_imu =  self.create_subscription(Imu, self.imu_topic, self._on_imu, qos_sensor)
         #Last log time
         self.last_log_t = 0.0
         #start time
@@ -73,13 +76,15 @@ class StepInputNode(Node):
             log_dir = os.path.expanduser('/workspace/logs')
             os.makedirs(log_dir, exist_ok=True)
             self.log_file = open(os.path.join(log_dir, 'speed_log.csv'), 'w')
-            self.log_file.write("time,cmd_vel_linear,speed\n")
+            self.log_file.write("time,cmd_v,cmd_z,left_wheel,right_wheel,odom_yaw_rate,imu_yaw_rate\n")
             self.get_logger().warn(f'Logging to: {self.log_file.name}')
         except Exception as e:
             self.get_logger().warn(f'Error in logging: {e}')
 
-        self.counter = 0
-        self.latest_speed = 0.0
+        self.joint_left = 0.0
+        self.joint_right = 0.0
+        self.odom_z = 0.0
+        self.yaw_rate = 0.0
 
     #Calculates the time from startup till now    
     def _now_sec(self):
@@ -94,12 +99,17 @@ class StepInputNode(Node):
             msg.linear.x = self.step_amplitude
             msg.angular.z = self.angular_z
         else:
-            msg.linear.x = 0.0
+            msg.linear.x = self.step_amplitude
             msg.angular.z = 0.0
         cmd_v = msg.linear.x
+        cmd_z = msg.angular.z
         self.publisher.publish(msg)
-        self.log_file.write(f"{t},{cmd_v},{self.latest_speed}\n")
+        self.log_file.write(f"{t},{cmd_v},{cmd_z},{self.joint_left},{self.joint_right},{self.odom_z},{self.yaw_rate}\n")
         self.log_file.flush()
+
+    def _on_imu(self,msg):
+        self.yaw_rate = msg.angular_velocity.z
+        self.get_logger().info(f'yaw rate = {self.yaw_rate}')
 
     def _on_joint_states(self, msg):
         # JointState har: msg.name, msg.position, msg.velocity, msg.effort
@@ -108,25 +118,22 @@ class StepInputNode(Node):
         
         # Tag gennemsnittet for en samlet fremad-hastighed:
         if len(msg.velocity) >= 2:
-            left = msg.velocity[0]
-            right = msg.velocity[1]
-            self.get_logger().info(f"left={left:.5f}, right={right:.5f}")
-            speed = (left + right) / 2.0   # rad/s
+            self.joint_left = msg.velocity[0]
+            self.joint_right = msg.velocity[1]
+            self.get_logger().info(f"left={self.joint_left:.5f}, right={self.joint_right:.5f}")
+            speed = (self.joint_left + self.joint_right) / 2.0   # rad/s
             t = self._now_sec()
-            self.latest_speed = speed
+            self.joint_speed = speed
 
     def _on_odom(self, msg):
         v = msg.twist.twist.linear
+        self.odom_z = msg.twist.twist.angular
         speed = np.sqrt(v.x**2 + v.y**2 + v.z**2)
         t = self._now_sec()
-        if self.counter % 5 == 0:
-            self.get_logger().info(f't={t:5.2f}s, speed = {speed:5.3f}m/s')
-        if t - self.last_log_t > 0.02: # log ca 50 Hz
-            #self.get_logger().info(f't={t:5.2f}s, speed = {speed:5.3f}m/s')
-            self.log_file.write(f"{t},{speed}\n")
-            self.log_file.flush()
-            self.last_log_t = t
-            self.counter += 1
+        self.get_logger().info(f't={t:5.2f}s, speed = {speed:5.3f}m/s, angular = {self.odom_z:5.3f}')
+        # self.log_file.write(f"{t},{speed},{z}\n")
+        self.log_file.flush()
+        self.last_log_t = t
     
     def destroy_node(self):
         self.log_file.close()
