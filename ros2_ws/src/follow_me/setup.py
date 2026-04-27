91a9c967812b0ca4a19f1e3b675e09a39ac5300f
TacoTorben <oliverfast18@gmail.com>
2026-04-27 12:08:19 +0200

    ADD: new pkg for full integration of follow_me system

---
+++ ros2_ws/src/follow_me/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'follow_me'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ROB4',
+    maintainer_email='oliverfast18@gmail.com',
+    description='Follow-me YOLO system for Jetson Orin Nano and Realsense D435i',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "person_center = follow_me_yolo.yolo_person_center:main",
+        ],
+    },
+)
