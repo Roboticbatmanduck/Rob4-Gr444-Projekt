86db06f9b506bddb44c3816b758f8abc76429195
roboticbatmanduck <jlinde24@student.aau.dk>
2026-05-13 15:31:53 +0200

    fix: comment out timeout check in compute_control method

--- ros2_ws/src/follow_me/follow_me/angle_regulator.py
+++ ros2_ws/src/follow_me/follow_me/angle_regulator.py
@@ -126,8 +126,8 @@ class AngleRegulator (Node):
         #Regulatoren altså PID/Lead lag led indsættes her
         dt = self.get_clock().now() - self.last_msg
         seconds_since_last_msg = dt.nanoseconds * 1e-9
-        if seconds_since_last_msg > self.timeout:
-             return 0.0
+        # if seconds_since_last_msg > self.timeout:
+        #      return 0.0
         # if abs(self.error) < self.deadband:
         #     return self.u_prev
         T = self.period
